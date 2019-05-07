3550db0614bbc1c85f7b84d782b43567e36ad303
Yevgen Goliuk <evgeniy.goliuk@sysgears.com>
2019-05-07 13:00:32 +0300

    Split dataset as DataFrame

--- Exercise_3/Naive_Bayes_text_classification/utils.py
+++ Exercise_3/Naive_Bayes_text_classification/utils.py
@@ -17,6 +17,7 @@ def split_dataset(data):
     for label in labels:
         ds_with_label = dataset[dataset['labels'] == label]
         num_test = int(ds_with_label.shape[0]*0.2)
+        num_test = 1 if num_test < 1 and ds_with_label.shape[0] > 1 else num_test
         ds_test = ds_with_label[:num_test]
         ds_train = ds_with_label[num_test:]
         test_datasets=pd.concat((test_datasets, ds_test))
@@ -31,3 +32,23 @@ def split_dataset(data):
     test_labels = np.array(test_datasets['labels']).reshape(test_datasets.shape[0], 1)
 
     return train_messages, train_labels, test_messages, test_labels
+
+def split_dataset_data_frame(data):
+    dataset = data.copy(deep=True)
+    labels = dataset['labels'].unique()
+    test_datasets = pd.DataFrame()
+    train_datasets = pd.DataFrame()
+
+    for label in labels:
+        ds_with_label = dataset[dataset['labels'] == label]
+        num_test = int(ds_with_label.shape[0]*0.2)
+        num_test = 1 if num_test < 1 and ds_with_label.shape[0] > 1 else num_test
+        ds_test = ds_with_label[:num_test]
+        ds_train = ds_with_label[num_test:]
+        test_datasets=pd.concat((test_datasets, ds_test))
+        train_datasets=pd.concat((train_datasets, ds_train))
+
+    test_datasets = sklearn_shuffle(test_datasets, random_state=0)
+    train_datasets = sklearn_shuffle(train_datasets, random_state=0)
+
+    return train_datasets, test_datasets
